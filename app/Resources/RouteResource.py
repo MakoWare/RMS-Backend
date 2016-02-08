f9a4bb219b4cf421a018db5452a67a531f868e01
yintothayang <yintothayang@gmail.com>
2016-02-07 18:24:02 -0800

    Walls and Routes CRUD

--- app/Resources/RouteResource.py
+++ app/Resources/RouteResource.py
@@ -7,9 +7,9 @@ from flask.ext.restful import abort, Resource, fields, marshal_with
 import datetime
 import json
 
-class GymResource(Resource):
+class RouteResource(Resource):
     def get(self, id):
-        resource = db.gyms.find_one({"_id": ObjectId(id)})
+        resource = db.routes.find_one({"_id": ObjectId(id)})
         if not resource:
             abort(404, message="does not exist, or has been soft deleted".format(id))
         return json.loads(dumps(resource))
@@ -17,24 +17,24 @@ class GymResource(Resource):
     def put(self, id):
         requestJSON = request.get_json()
         resourceId = {'_id': ObjectId(id)}
-        updatedResource = db.gyms.find_one_and_update(resourceId, {"$set": requestJSON}, return_document=ReturnDocument.AFTER)
+        updatedResource = db.routes.find_one_and_update(resourceId, {"$set": requestJSON}, return_document=ReturnDocument.AFTER)
         return json.loads(dumps(updatedResource)), 201
 
     def delete(self, id):
         now = datetime.datetime.utcnow()
         softDelete = {"deletedAt": now}
         resourceId = {'_id': ObjectId(id)}
-        updatedResource = db.gyms.find_one_and_update(resourceId, {"$set": softDelete}, return_document=ReturnDocument.AFTER)
+        updatedResource = db.routes.find_one_and_update(resourceId, {"$set": softDelete}, return_document=ReturnDocument.AFTER)
         return json.loads(dumps(updatedResource)), 201
 
-class GymListResource(Resource):
+class RouteListResource(Resource):
     def get(self):
-        gyms = db.gyms.find_one()
-        return json.loads(dumps(gyms)), 201
+        resources = db.resources.find_one()
+        return json.loads(dumps(resources)), 201
 
     def post(self):
         requestJSON = request.get_json()
-        createdResourceId = db.gyms.insert_one(requestJSON).inserted_id
-        createdResource = db.gyms.find_one(createdResourceId)
+        createdResourceId = db.routes.insert_one(requestJSON).inserted_id
+        createdResource = db.routes.find_one(createdResourceId)
         return json.loads(dumps(createdResource))
 
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,22 @@
 from app.Resources.UserResource import *
 from app.Resources.GymResource import *
+from app.Resources.WallResource import *
+from app.Resources.RouteResource import *
 from app import *
 
 
-#Users 
+#Users
 api.add_resource(UserListResource, '/users', endpoint='users')
 api.add_resource(UserResource, '/users/<id>', endpoint='user')
 
 #Gyms
 api.add_resource(GymListResource, '/gyms', endpoint='gyms')
 api.add_resource(GymResource, '/gyms/<id>', endpoint='gym')
+
+#Walls
+api.add_resource(WallListResource, '/walls', endpoint='walls')
+api.add_resource(WallResource, '/walls/<id>', endpoint='wall')
+
+#Routes
+api.add_resource(RouteListResource, '/routes', endpoint='routes')
+api.add_resource(RouteResource, '/routes/<id>', endpoint='route')
